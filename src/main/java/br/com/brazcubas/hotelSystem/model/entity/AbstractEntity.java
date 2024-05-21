58d83a980d68ee397dfd888fe961e5b2da395222
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2024-05-21 19:17:43 -0300

    Delete src directory

--- src/main/java/br/com/brazcubas/hotelSystem/model/entity/AbstractEntity.java
+++
@@ -1,8 +0,0 @@
-package br.com.brazcubas.hotelSystem.model.entity;
-
-public class AbstractEntity {
-    private int id;
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-}
