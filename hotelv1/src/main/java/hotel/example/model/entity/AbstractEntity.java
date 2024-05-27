b74edd169fc37e781e22e545281c6451b67dccb2
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2024-05-27 00:17:03 -0300

    Delete hotelv1 directory

--- hotelv1/src/main/java/hotel/example/model/entity/AbstractEntity.java
+++
@@ -1,8 +0,0 @@
-package hotel.example.model.entity;
-
-public class AbstractEntity {
-    private int id;
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-}
